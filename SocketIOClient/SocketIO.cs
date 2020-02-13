08096ef5f10d8560501fd4a30cf6d0648b1571ab
winwin <yjh2011@live.com>
2020-02-13 14:05:37 +0800

    修改接口，兼容旧版本

--- SocketIOClient/SocketIO.cs
+++ SocketIOClient/SocketIO.cs
@@ -13,17 +13,27 @@ namespace SocketIOClient
     public class SocketIO
     {
         /// <summary>
-        /// 
+        /// Options 1: http://example.com/subpath + /socket.io + /sionamespace + ture
+        /// Options 2: http://example.com/sionamespace + param path unuse + param sionamespace unuse + ture
         /// </summary>
-        /// <param name="uri">HTTP(s)服务地址，结尾不需要斜杠。例如：https://example.com/subpath</param>
+        /// <param name="uri">新版：HTTP(s)服务地址，结尾不需要斜杠。例如：https://example.com/subpath。旧版：http://example.com/sionamespace</param>
         /// <param name="path">Socket.io服务路径。socket.io在服务端的参数path，默认为/socket.io。例如：/subpath/which/provide/socket/io/service。参考https://socket.io/docs/server-api/</param>
-        /// <param name="sionamespace">命名空间，参考https://socket.io/docs/rooms-and-namespaces/</param>
-        public SocketIO(Uri uri, string path, string sionamespace)
+        /// <param name="sionamespace">命名空间，若不使用传入空字符串即可。参考https://socket.io/docs/rooms-and-namespaces/</param>
+        /// <param name="usePath">兼容选项，false时兼容旧版本</param>
+        public SocketIO(Uri uri, string path, string sionamespace, bool usePath)
         {
 
             if (uri.Scheme == "https" || uri.Scheme == "http" || uri.Scheme == "wss" || uri.Scheme == "ws")
             {
-                _uri = new Uri(uri.AbsoluteUri + path);
+                // uri = http://example.com:1234/sionamespace
+                // path = /path
+                // uri.Scheme = http
+                // uri.Authority = example.com:1234
+                // _uri = http://example.com:1234/path
+                if (!usePath)
+                    _uri = new Uri(uri.Scheme + "://" + uri.Authority + path);
+                else
+                    _uri = new Uri(uri.AbsoluteUri + path);
             }
             else
             {
@@ -33,16 +43,70 @@ namespace SocketIOClient
             Callbacks = new Dictionary<int, EventHandler>();
             _urlConverter = new UrlConverter();
 
-            if (sionamespace.Length > 0)
+            if (!usePath)
+            {
+                if (uri.AbsolutePath != "/")
+                    _namespace = uri.AbsolutePath + ',';
+            }
+            else if (sionamespace.Length > 0)
             {
                 _namespace = sionamespace + ',';
             }
+
             _packetId = -1;
             ConnectTimeout = TimeSpan.FromSeconds(30);
         }
 
-        public SocketIO(string uri) : this(new Uri(uri), "/socket.io", "") { }
-        public SocketIO(string uri, string path) : this(new Uri(uri), path, "") { }
+        /// <summary>
+        /// http://example.com/sionamespace
+        /// </summary>
+        /// <param name="uri">http://example.com/sionamespace</param>
+        public SocketIO(string uri) : this(new Uri(uri)) { }
+        /// <summary>
+        /// http://example.com/sionamespace
+        /// </summary>
+        /// <param name="uri">http://example.com/sionamespace</param>
+        public SocketIO(Uri uri) : this(uri, false) { }
+        /// <summary>
+        ///  Options 1: http://example.com/sionamespace + false
+        ///  Options 2: http://example.com/subpath + true
+        /// </summary>
+        /// <param name="uri">http://example.com/sionamespace or http://example.com/subpath</param>
+        /// <param name="usePath">false or true</param>
+        public SocketIO(string uri, bool usePath) : this(new Uri(uri), usePath) { }
+        /// <summary>
+        ///  Options 1: http://example.com/sionamespace + false
+        ///  Options 2: http://example.com/subpath + true
+        /// </summary>
+        /// <param name="uri">http://example.com/sionamespace or http://example.com/subpath</param>
+        /// <param name="usePath">false or true</param>
+        public SocketIO(Uri uri, bool usePath) : this(uri, "/socket.io", "", usePath) { }
+        /// <summary>
+        /// http://example.com/subpath + /sionamespace
+        /// </summary>
+        /// <param name="uri">http://example.com/subpath</param>
+        /// <param name="sionamespace">/sionamespace</param>
+        public SocketIO(string uri, string sionamespace) : this(new Uri(uri), sionamespace) { }
+        /// <summary>
+        /// http://example.com/subpath + /sionamespace
+        /// </summary>
+        /// <param name="uri">http://example.com/subpath</param>
+        /// <param name="sionamespace">/sionamespace</param>
+        public SocketIO(Uri uri, string sionamespace) : this(uri, "/socket.io", sionamespace, true) { }
+        /// <summary>
+        /// http://example.com/subpath + /socket.io + /sionamespace
+        /// </summary>
+        /// <param name="uri">http://example.com/subpath</param>
+        /// <param name="path">/socket.io</param>
+        /// <param name="sionamespace">/sionamespace</param>
+        public SocketIO(string uri, string path, string sionamespace) : this(new Uri(uri), path, sionamespace, true) { }
+        /// <summary>
+        /// http://example.com/subpath + /socket.io + /sionamespace
+        /// </summary>
+        /// <param name="uri">http://example.com/subpath</param>
+        /// <param name="path">/socket.io</param>
+        /// <param name="sionamespace">/sionamespace</param>
+        public SocketIO(Uri uri, string path, string sionamespace) : this(uri, path, sionamespace, true) { }
 
         private const int ReceiveChunkSize = 1024;
         private const int SendChunkSize = 1024;
