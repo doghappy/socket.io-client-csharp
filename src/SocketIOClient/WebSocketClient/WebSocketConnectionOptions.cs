f305be910507bc9329dfc2030eaa7ba0f4be1841
Kim <kim.schneider@server-eye.de>
2020-10-02 12:21:37 +0200

    Support WebProxy

--- src/SocketIOClient/WebSocketClient/WebSocketConnectionOptions.cs
+++ src/SocketIOClient/WebSocketClient/WebSocketConnectionOptions.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Net;
 
 namespace SocketIOClient.WebSocketClient
 {
     public class WebSocketConnectionOptions
     {
         public TimeSpan ConnectionTimeout { get; set; }
+        public IWebProxy Proxy { get; set; } = null;
     }
 }
