4a6ce831846ba1cf65e178d6eff862bec9183163
HeroWong <hero_wong@outlook.com>
2021-04-29 16:45:45 +0800

    feat: implement for Newtonsoft.Json

---
+++ src/SocketIOClient.Newtonsoft.Json/ByteArrayConverter.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SocketIOClient.Newtonsoft.Json
+{
+    class ByteArrayConverter : JsonConverter
+    {
+        public ByteArrayConverter(int eio)
+        {
+            this.eio = eio;
+            Bytes = new List<byte[]>();
+        }
+
+        readonly int eio;
+        internal IList<byte[]> Bytes { get; }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(byte[]);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, global::Newtonsoft.Json.JsonSerializer serializer)
+        {
+            byte[] bytes = null;
+            if (reader.TokenType == JsonToken.StartObject)
+            {
+                reader.Read();
+                if (reader.TokenType == JsonToken.PropertyName && reader.Value?.ToString() == "_placeholder")
+                {
+                    reader.Read();
+                    if (reader.TokenType == JsonToken.Boolean && (bool)reader.Value)
+                    {
+                        reader.Read();
+                        if (reader.TokenType == JsonToken.PropertyName && reader.Value?.ToString() == "num")
+                        {
+                            reader.Read();
+                            if (reader.Value != null)
+                            {
+                                if (int.TryParse(reader.Value.ToString(), out int num))
+                                {
+                                    bytes = Bytes[num];
+                                    reader.Read();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return bytes;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, global::Newtonsoft.Json.JsonSerializer serializer)
+        {
+            var source = (value as byte[]).ToList();
+            if (eio != 4)
+                source.Insert(0, 4);
+            Bytes.Add(source.ToArray());
+            writer.WriteStartObject();
+            writer.WritePropertyName("_placeholder");
+            writer.WriteValue(true);
+            writer.WritePropertyName("num");
+            writer.WriteValue(Bytes.Count - 1);
+            writer.WriteEndObject();
+        }
+    }
+}
