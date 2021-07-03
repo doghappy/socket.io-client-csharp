07d5fa08f4e24df2449c4bbe6497c3e659030355
jlareo <lareomerlin@gmail.com>
2021-07-03 22:19:14 +0200

    feat: Add logic to run the different nodejs servers before the tests. To avoid making a lot of changes in the tests, the logic is performed by using AssemblyInitialize and the AssemblyCleanup instead of TestInitialize and TestCleanup (but could be done if desired). In the future, the tests could be separated in different projects (e.g. UnitTests and IntegrationTests), so, doing the configuration of the different servers should be done only by running the integration tests.

---
+++ src/SocketIOClient.Test/SocketIOTests/AssemblyIntegrationTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocketIOClient.Test.SocketIOTests.V4;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SocketIOClient.Test.SocketIOTests
+{
+    [TestClass]
+
+    public class AssemblyIntegrationTest
+    {
+        private static readonly IEnumerable<IServerManager> Servers = new List<IServerManager>()
+        {
+            new ServerV2Manager(),
+            new ServerV3Manager(),
+            new ServerV4Manager()
+        };
+
+        [AssemblyInitialize]
+        public static void Initialize(TestContext context)
+        {
+            foreach (var server in Servers) 
+            {
+                server.Create();
+            }
+
+            // Give some time to be sure that servers are running.
+            Thread.Sleep(5000);
+        }
+
+        [AssemblyCleanup]
+        public static void Cleanup()
+        {
+            foreach (var server in Servers)
+            {
+                server.Destroy();
+            }
+        }
+    }
+}
---
+++ src/SocketIOClient.Test/SocketIOTests/BaseServerManager.cs
@@ -0,0 +1,50 @@
+﻿using SocketIOClient.Test.Helpers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace SocketIOClient.Test.SocketIOTests
+{
+    public class BaseServerManager
+    {
+        private readonly string directory;
+        private List<Process> processesToKill;
+
+        public BaseServerManager(string directory) 
+        {
+            this.directory = directory;
+        }
+
+        public void Create()
+        {
+            var installProcess = CommandHelper.RunCommand("npm", "install", Path.GetFullPath(directory));
+            
+            if (!installProcess.WaitForExit(60000)) 
+            {
+                throw new System.SystemException("Failed restoring packages of server");
+            }
+
+            var before = Process.GetProcesses().ToList().Select(x => x.Id);
+            var startProcess = CommandHelper.RunCommand("npm", "start", Path.GetFullPath(directory));
+            var after = Process.GetProcesses().ToList();
+            this.processesToKill = this.GetProcessesToKill(before, after);
+        }
+
+        public void Destroy()
+        {
+            foreach (var process in processesToKill)
+            {
+                process.Kill();
+            }
+        }
+
+        private List<Process> GetProcessesToKill(IEnumerable<int> before, List<Process> after)
+        {
+            var processesToKill = after;
+            processesToKill.RemoveAll(x => before.Contains(x.Id) && !x.ProcessName.Contains("npm"));
+            return processesToKill;
+        }
+    }
+}
