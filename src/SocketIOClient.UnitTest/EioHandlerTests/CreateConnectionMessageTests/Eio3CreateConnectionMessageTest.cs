31d7666cb926b94dc76968f77aca1fd0eaf24483
HeroWong <hero_wong@outlook.com>
2021-07-15 14:30:50 +0800

    test: create unit tests
    [skip ci]

---
+++ src/SocketIOClient.UnitTest/EioHandlerTests/CreateConnectionMessageTests/Eio3CreateConnectionMessageTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocketIOClient.EioHandler;
+using System.Collections.Generic;
+
+namespace SocketIOClient.UnitTest.EioHandlerTests.CreateConnectionMessageTests
+{
+    [TestClass]
+    public class Eio3CreateConnectionMessageTest
+    {
+        [TestMethod]
+        public void NullNamespaceNullQuery()
+        {
+            var handler = new Eio3Handler();
+            string message = handler.CreateConnectionMessage(null, null);
+            Assert.AreEqual("40", message);
+        }
+
+        [TestMethod]
+        public void NullNamespaceEmptyQuery()
+        {
+            var handler = new Eio3Handler();
+            string message = handler.CreateConnectionMessage(null, new Dictionary<string, string>());
+            Assert.AreEqual("40", message);
+        }
+
+        [TestMethod]
+        public void NullNamespace1Query()
+        {
+            var handler = new Eio3Handler();
+            string message = handler.CreateConnectionMessage(null, new Dictionary<string, string>
+            {
+                { "key", "val" }
+            });
+            Assert.AreEqual("40?key=val", message);
+        }
+
+        [TestMethod]
+        public void Namespace2Query()
+        {
+            var handler = new Eio3Handler();
+            string message = handler.CreateConnectionMessage("/nsp", new Dictionary<string, string>
+            {
+                { "key", "val" },
+                { "token", "V2" }
+            });
+            Assert.AreEqual("40/nsp?key=val&token=V2,", message);
+        }
+    }
+}
