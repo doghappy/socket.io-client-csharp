ffe51bfb55d9fcc06c53a6df9d325564c6e7195c
HeroWong <hero_wong@outlook.com>
2021-07-15 16:25:26 +0800

    test: OnTextReceived

---
+++ src/SocketIOClient.UnitTest/SocketIOTests/OnTextReceivedTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SocketIOClient.Processors;
+using SocketIOClient.WebSocketClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketIOClient.UnitTest.SocketIOTests
+{
+    [TestClass]
+    public class OnTextReceivedTest
+    {
+        [TestMethod]
+        public void Text()
+        {
+            var mockProcessor = new Mock<Processor>();
+            var mockWebSocketClient = new Mock<IWebSocketClient>();
+            //mockWebSocketClient.SetupAllProperties();
+            mockWebSocketClient.SetupProperty(x => x.OnTextReceived);
+            var io = new SocketIO
+            {
+                MessageProcessor = mockProcessor.Object,
+                Socket = mockWebSocketClient.Object
+            };
+            mockWebSocketClient.Object.OnTextReceived("test");
+            mockProcessor.Verify(p => p.Process(It.IsAny<MessageContext>()), Times.Once());
+            mockWebSocketClient.Verify(p => p.OnTextReceived, Times.Once());
+            mockWebSocketClient.Verify(p => p.OnBinaryReceived, Times.Never());
+        }
+    }
+}
