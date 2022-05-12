b5d06b947dae3736c512ed953ca616ed2ca72a86
doghappy <hero_wong@outlook.com>
2022-05-12 21:29:12 +0800

    delete smaple project and add integration tests

---
+++ src/SocketIOClient.IntegrationTests/SocketIOTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SocketIOClient.IntegrationTests
+{
+    [TestClass]
+    public class SocketIOTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
