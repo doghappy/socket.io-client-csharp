f61666e29c2fb22b53e688d4fce3677bb2947934
doghappy <hero_wong@outlook.com>
2022-09-25 21:16:58 +0800

    clean code

--- src/SocketIOClient.Test/Configuration/Preference.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SocketIOClient.Test.Configuration
-{
-    public class Preference
-    {
-        public bool RunServers { get; set; }
-        public bool StopServersAfterRun { get; set; }
-    }
-}
