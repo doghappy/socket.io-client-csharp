532a316d3dfbee6c25ef16633a939ed9ba552e99
doghappy <hero_wong@outlook.com>
2022-10-31 20:24:37 +0800

    fix pipeline

---
+++ azure-devops/check-port.ps1
@@ -0,0 +1,23 @@
+$ports = 11400, 11410, 11401, 11411, 11300, 11310, 11301, 11311, 11200, 11210, 11201, 11211
+function Test-SocketIOConnection($Port) {
+    for ($i = 0; $i -lt 3; $i++) {
+        $result = Test-Connection -TargetName localhost -TcpPort $Port
+        if ($result) {
+            Write-Host "$Port opened"
+            return true
+        }
+        else {
+            Write-Host "$port not open, will retry after 3 s..."
+            Start-Sleep -Seconds 3
+        }
+    }
+    Write-Host "$port is not open after 3 retries."
+    return false
+}
+
+foreach ($port in $ports) {
+    $result = Test-SocketIOConnection -Port $port
+    if (!$result) {
+        throw "All socket.io erver ports must be open"
+    }
+}
