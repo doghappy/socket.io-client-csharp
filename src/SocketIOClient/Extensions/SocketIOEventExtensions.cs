68abd98d2770d44a0c8634d5d62d3eb7abb2b9b4
doghappy <hero_wong@outlook.com>
2023-08-13 22:16:25 +0800

    Run user's code in background

--- src/SocketIOClient/Extensions/SocketIOEventExtensions.cs
+++ src/SocketIOClient/Extensions/SocketIOEventExtensions.cs
@@ -7,38 +7,17 @@ namespace SocketIOClient.Extensions
     {
         public static void TryInvoke(this OnAnyHandler handler, string eventName, SocketIOResponse response)
         {
-            try
-            {
-                handler(eventName, response);
-            }
-            catch
-            {
-                // The exception is thrown by the user code, so it can be swallowed
-            }
+            Task.Run(() => handler(eventName, response));
         }
 
         public static void TryInvoke(this Action<SocketIOResponse> handler, SocketIOResponse response)
         {
-            try
-            {
-                handler(response);
-            }
-            catch
-            {
-                // The exception is thrown by the user code, so it can be swallowed
-            }
+            Task.Run(() => handler(response));
         }
 
-        public static async Task TryInvoke(this Func<SocketIOResponse, Task> handler, SocketIOResponse response)
+        public static Task TryInvoke(this Func<SocketIOResponse, Task> handler, SocketIOResponse response)
         {
-            try
-            {
-                await handler(response);
-            }
-            catch
-            {
-                // The exception is thrown by the user code, so it can be swallowed
-            }
+            return handler(response);
         }
     }
 }
--- tests/SocketIOClient.IntegrationTests/V4WebSocketTests.cs
+++ tests/SocketIOClient.IntegrationTests/V4WebSocketTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -24,15 +25,15 @@ namespace SocketIOClient.IntegrationTests
             using var io = CreateSocketIO();
             var results = new List<int>();
             io.On("1:emit", res => results.Add(6 / res.GetValue<int>()));
-        
+
             await io.ConnectAsync();
             await io.EmitAsync("1:emit", 0);
             await io.EmitAsync("1:emit", 1);
             await Task.Delay(100);
-        
+
             results.Should().Equal(6);
         }
-        
+
         [TestMethod]
         public async Task Should_Throw_Exception_If_Proxy_Server_Not_Start()
         {
@@ -46,7 +47,7 @@ namespace SocketIOClient.IntegrationTests
                 .Should()
                 .ThrowAsync<ConnectionException>();
         }
-        
+
         [TestMethod]
         public async Task Should_Able_To_Connect_To_Proxy()
         {
@@ -62,11 +63,11 @@ namespace SocketIOClient.IntegrationTests
             await io.Invoking(async x => await x.ConnectAsync())
                 .Should()
                 .ThrowAsync<ConnectionException>();
-        
+
             var uri = new Uri(Common.Startup.V4_NSP_WS);
             var uriStr = $"{uri.Host}:{uri.Port}";
             msgs.Should().BeEquivalentTo(new[] { $"CONNECT {uriStr} HTTP/1.1\r\nHost: {uriStr}\r\n\r\n" });
-        
+
             void StartProxyServer()
             {
                 var proxy = new TcpListener(IPAddress.Parse("127.0.0.1"), 6138);
@@ -83,6 +84,46 @@ namespace SocketIOClient.IntegrationTests
                     }
                 }
             }
+        }
+
+        [TestMethod]
+        public async Task Should_not_block_other_events_when_calling_Delay()
+        {
+            using var io = CreateSocketIO();
+            var results = new List<int>();
+            io.On("1:emit", async res =>
+            {
+                var n = res.GetValue<int>();
+                await Task.Delay(n);
+                results.Add(n);
+            });
+
+            await io.ConnectAsync();
+            await io.EmitAsync("1:emit", 2000);
+            await io.EmitAsync("1:emit", 1000);
+            await Task.Delay(3000);
+
+            results.Should().Equal(1000, 2000);
+        }
+
+        [TestMethod]
+        public async Task Should_not_block_other_events_when_calling_Sleep()
+        {
+            using var io = CreateSocketIO();
+            var results = new List<int>();
+            io.On("1:emit", res =>
+            {
+                var n = res.GetValue<int>();
+                Thread.Sleep(n);
+                results.Add(n);
+            });
+
+            await io.ConnectAsync();
+            await io.EmitAsync("1:emit", 2000);
+            await io.EmitAsync("1:emit", 1000);
+            await Task.Delay(3000);
+
+            results.Should().Equal(1000, 2000);
         }
     }
 }
