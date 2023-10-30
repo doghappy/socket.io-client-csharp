05650d3d67c2e4bcd58b7444c7c18684be41b84e
doghappy <hero_wong@outlook.com>
2023-10-30 21:53:24 +0800

    tring to fix random failures

--- tests/SocketIOClient.IntegrationTests/SocketIOTests.cs
+++ tests/SocketIOClient.IntegrationTests/SocketIOTests.cs
@@ -552,8 +552,8 @@ namespace SocketIOClient.IntegrationTests
 
         [TestMethod]
         [DataRow(4000, 0, 0)]
-        [DataRow(5100, 1, 1)]
-        [DataRow(10100, 2, 2)]
+        [DataRow(5900, 1, 1)]
+        [DataRow(14900, 2, 2)]
         public async Task Ping_pong_test(int ms, int expectedPingTimes, int expectedPongTimes)
         {
             var pingTimes = 0;
