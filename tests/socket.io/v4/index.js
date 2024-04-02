4ab6b46dbc2875a2a095b6d18139ac9380b9e433
doghappy <hero_wong@outlook.com>
2024-04-02 23:05:11 +0800

    refactor nodejs code

---
+++ tests/socket.io/v4/index.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const https = require('https');
+const http = require('http');
+const socket = require('socket.io');
+const fs = require('fs');
+const path = require('path');
+const template = require('./template');
+
+const wsServers = [
+    {
+        name: 'v4-ws',
+        port: 11400,
+        server: () => http.createServer(),
+        options: {
+            pingInterval: 5000,
+            pingTimeout: 10000,
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"]
+            }
+        },
+        onCreated: template.registerEvents
+    },
+    {
+        name: 'v4-ws-token',
+        port: 11401,
+        server: () => http.createServer(),
+        options: {},
+        onCreated: template.useAuthMiddlewares
+    },
+    {
+        name: 'v4-ws-mp',
+        port: 11402,
+        server: () => http.createServer(),
+        options: {
+            parser: require('socket.io-msgpack-parser'),
+            pingInterval: 5000,
+            pingTimeout: 10000,
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"]
+            }
+        },
+        onCreated: template.registerEvents
+    },
+    {
+        name: 'v4-ws-token-mp',
+        port: 11403,
+        server: () => http.createServer(),
+        options: {
+            parser: require('socket.io-msgpack-parser')
+        },
+        onCreated: template.useAuthMiddlewares
+    },
+    {
+        name: 'v4-ws-ssl-err',
+        port: 11404,
+        server: () => {
+            const server = https.createServer({
+                key: fs.readFileSync(path.join(__dirname, 'cert', 'private.key')),
+                cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.crt'))
+            }, (req, res) => {
+                if (req.method === 'GET' && req.url === '/hello') {
+                    res.end('Hello World!\n');
+                }
+            });
+            return server;
+        },
+        options: {
+            pingInterval: 5000,
+            pingTimeout: 10000,
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"]
+            }
+        },
+        onCreated: template.registerEvents
+    }
+];
+
+const httpServers = [
+    {
+        name: 'v4-http',
+        port: 11410,
+        server: () => http.createServer(),
+        options: {
+            transports: ["polling"],
+            pingInterval: 5000,
+            pingTimeout: 10000,
+        },
+        onCreated: template.registerEvents
+    },
+    {
+        name: 'v4-http-token',
+        port: 11411,
+        server: () => http.createServer(),
+        options: {
+            transports: ["polling"]
+        },
+        onCreated: template.useAuthMiddlewares
+    },
+    {
+        name: 'v4-http-mp',
+        port: 11412,
+        server: () => http.createServer(),
+        options: {
+            parser: require('socket.io-msgpack-parser'),
+            transports: ["polling"],
+            pingInterval: 5000,
+            pingTimeout: 10000,
+        },
+        onCreated: template.registerEvents
+    },
+    {
+        name: 'v4-http-token-mp',
+        port: 11413,
+        server: () => http.createServer(),
+        options: {
+            parser: require('socket.io-msgpack-parser'),
+            transports: ["polling"]
+        },
+        onCreated: template.useAuthMiddlewares
+    }
+];
+
+const servers = [
+    ...wsServers,
+    ...httpServers
+];
+
+for (const server of servers) {
+    console.log(`Starting server '${server.name}' on port ${server.port}...`);
+    template.start(server.name, server.port, server.server(), server.options, server.onCreated);
+}
--- tests/socket.io/v4/template.js
+++ tests/socket.io/v4/template.js
@@ -1,6 +1,46 @@
 'use strict';
 
+function start(name, port, server, options, onCreated) {
+    const socket = require('socket.io');
+    const io = socket(server, options);
+    onCreated(io);
+    server.listen(port, () => {
+        console.log(`Started server '${name}' on port ${port}`);
+    });
+}
+
+function useAuthMiddlewares(io){
+    useIOAuthMiddleware(io);
+    useNspAuthMiddleware(io);
+}
+
+function useIOAuthMiddleware(io){
+    io.use((socket, next) => {
+        if (socket.handshake.query.token === "abc") {
+            next();
+        } else {
+            next(new Error("Authentication error"));
+        }
+    });
+}
+
+function useNspAuthMiddleware(io){
+    const nsp = io.of("/nsp");
+    nsp.use((socket, next) => {
+        if (socket.handshake.query.token === "abc") {
+            next();
+        } else {
+            next(new Error("Authentication error"));
+        }
+    });
+}
+
 function registerEvents(io) {
+    registerIOEvents(io);
+    registerNspEvents(io);
+}
+
+function registerIOEvents(io) {
     io.on('connection', socket => {
         socket.on('1:emit', data => {
             socket.emit('1:emit', data);
@@ -26,7 +66,9 @@ function registerEvents(io) {
             });
         });
     });
+}
 
+function registerNspEvents(io) {
     const nsp = io.of("/nsp");
     nsp.on("connection", socket => {
         socket.on('1:emit', data => {
@@ -55,6 +97,9 @@ function registerEvents(io) {
     });
 }
 
-//TODO: need to refactor and reuse this function
-
-module.exports = registerEvents
+module.exports.start = start;
+module.exports.useAuthMiddlewares = useAuthMiddlewares;
+module.exports.useIOAuthMiddleware = useIOAuthMiddleware;
+module.exports.useNspAuthMiddleware = useNspAuthMiddleware;
+module.exports.registerEvents = registerEvents;
+module.exports.registerIOEvents = registerIOEvents;
