0be39728ce72eafb00776e4ffb3679fede749aa1
doghappy <hero_wong@outlook.com>
2025-01-26 23:31:30 +0800

    eio bytes to http req

--- src/SocketIOClient/V2/Session/EngineIOHttpAdapter/EngineIO3Adapter.cs
+++ src/SocketIOClient/V2/Session/EngineIOHttpAdapter/EngineIO3Adapter.cs
@@ -13,13 +13,30 @@ public class EngineIO3Adapter : IEngineIOAdapter
         {
             throw new ArgumentException("The array cannot be empty");
         }
+        if (bytes.Any(b => b.Length == 0))
+        {
+            throw new ArgumentException("The sub array cannot be empty");
+        }
+        var req = new HttpRequest
+        {
+            Method = RequestMethod.Post,
+            BodyType = RequestBodyType.Bytes,
+            Headers = new Dictionary<string, string>
+            {
+                { HttpHeaders.ContentType, MediaTypeNames.Application.Octet },
+            },
+        };
+        var capacity = bytes.Sum(x => x.Length + 16);
+        var list = new List<byte>(capacity);
         foreach (var b in bytes)
         {
-            if (b.Length == 0)
-            {
-                throw new ArgumentException("The sub array cannot be empty");
-            }
+            list.Add(1);
+            list.AddRange(b.Length.ToString().Select(c => Convert.ToByte(c - '0')));
+            list.Add(byte.MaxValue);
+            list.Add(4);
+            list.AddRange(b);
         }
-        throw new System.NotImplementedException();
+        req.BodyBytes = list.ToArray();
+        return req;
     }
 }
--- tests/SocketIOClient.UnitTests/V2/Session/EngineIOHttpAdapter/EngineIO3AdapterTests.cs
+++ tests/SocketIOClient.UnitTests/V2/Session/EngineIOHttpAdapter/EngineIO3AdapterTests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
+using SocketIOClient.V2.Protocol.Http;
 using SocketIOClient.V2.Session.EngineIOHttpAdapter;
 using Xunit;
 
@@ -8,8 +10,8 @@ namespace SocketIOClient.UnitTests.V2.Session.EngineIOHttpAdapter;
 
 public class EngineIO3AdapterTests
 {
-    private readonly EngineIO3Adapter _adapter = new(); 
-    
+    private readonly EngineIO3Adapter _adapter = new();
+
     [Fact]
     public void ToHttpRequest_GivenAnEmptyArray_ThrowException()
     {
@@ -19,7 +21,7 @@ public class EngineIO3AdapterTests
             .Throw<ArgumentException>()
             .WithMessage("The array cannot be empty");
     }
-    
+
     [Fact]
     public void ToHttpRequest_GivenAnEmptySubBytes_ThrowException()
     {
@@ -33,6 +35,78 @@ public class EngineIO3AdapterTests
             .Throw<ArgumentException>()
             .WithMessage("The sub array cannot be empty");
     }
-    
-    // TODO: Add more tests
+
+    private static readonly (ICollection<byte[]> bytes, IHttpRequest req) OneItem1Byte = new(new List<byte[]>
+    {
+        new byte[] { 1 },
+    }, new HttpRequest
+    {
+        BodyBytes =
+        [
+            1, 1, 255, 4, 1,
+        ],
+        Method = RequestMethod.Post,
+        BodyType = RequestBodyType.Bytes,
+        Headers = new Dictionary<string, string>
+        {
+            { "Content-Type", "application/octet-stream" },
+        },
+    });
+
+    private static readonly (ICollection<byte[]> bytes, IHttpRequest req) OneItem10Bytes = new(new List<byte[]>
+    {
+        Enumerable.Range(0, 10).Select(x => (byte)x).ToArray(),
+    }, new HttpRequest
+    {
+        BodyBytes =
+        [
+            1, 1, 0, 255, 4, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        ],
+        Method = RequestMethod.Post,
+        BodyType = RequestBodyType.Bytes,
+        Headers = new Dictionary<string, string>
+        {
+            { "Content-Type", "application/octet-stream" },
+        },
+    });
+
+    private static readonly (ICollection<byte[]> bytes, IHttpRequest req) TwoItems1Byte10Bytes = new(new List<byte[]>
+    {
+        new byte[] { 1 },
+        Enumerable.Range(0, 10).Select(x => (byte)x).ToArray(),
+    }, new HttpRequest
+    {
+        BodyBytes =
+        [
+            1, 1, 255, 4, 1,
+            1, 1, 0, 255, 4, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        ],
+        Method = RequestMethod.Post,
+        BodyType = RequestBodyType.Bytes,
+        Headers = new Dictionary<string, string>
+        {
+            { "Content-Type", "application/octet-stream" },
+        },
+    });
+
+    private static IEnumerable<(ICollection<byte[]> bytes, IHttpRequest req)> ToHttpRequestStrongTypeCases
+    {
+        get
+        {
+            yield return OneItem1Byte;
+            yield return OneItem10Bytes;
+            yield return TwoItems1Byte10Bytes;
+        }
+    }
+
+    public static IEnumerable<object[]> ToHttpRequestCases =>
+        ToHttpRequestStrongTypeCases.Select(x => new object[] { x.bytes, x.req });
+
+    [Theory]
+    [MemberData(nameof(ToHttpRequestCases))]
+    public void ToHttpRequest_WhenCalled_AlwaysPass(ICollection<byte[]> bytes, IHttpRequest result)
+    {
+        var req = _adapter.ToHttpRequest(bytes);
+        req.Should().BeEquivalentTo(result);
+    }
 }
