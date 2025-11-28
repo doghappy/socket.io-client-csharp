d45b86cb01e1b48b1805c046ba95462e0c80ae36
doghappy <hero_wong@outlook.com>
2025-11-28 23:09:19 +0800

    implement ConnectAsync for WebSocketSession.cs

---
+++ src/SocketIOClient/V2/Session/WebSocket/WebSocketSession.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SocketIOClient.Core;
+using SocketIOClient.V2.Protocol.WebSocket;
+using SocketIOClient.V2.UriConverter;
+
+namespace SocketIOClient.V2.Session.WebSocket;
+
+public class WebSocketSession(
+    ILogger<WebSocketSession> logger,
+    IWebSocketAdapter wsAdapter,
+    IUriConverter uriConverter) : SessionBase(logger, uriConverter, wsAdapter)
+{
+    protected override Core.Protocol Protocol => Core.Protocol.WebSocket;
+
+    public override Task OnNextAsync(ProtocolMessage message)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void OnOptionsChanged(SessionOptions newValue)
+    {
+    }
+
+    public override Task SendAsync(object[] data, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task SendAsync(object[] data, int packetId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task SendAckDataAsync(object[] data, int packetId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override async Task ConnectCoreAsync(Uri uri, CancellationToken cancellationToken)
+    {
+        await wsAdapter.ConnectAsync(uri, cancellationToken).ConfigureAwait(false);
+    }
+
+    public override Task DisconnectAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/SocketIOClient.UnitTests/V2/Session/WebSocket/WebSocketSessionTests.cs
@@ -0,0 +1,121 @@
+using System.Net.WebSockets;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using SocketIOClient.Core;
+using SocketIOClient.Test.Core;
+using SocketIOClient.V2.Protocol.WebSocket;
+using SocketIOClient.V2.Session;
+using SocketIOClient.V2.Session.WebSocket;
+using SocketIOClient.V2.UriConverter;
+using Xunit.Abstractions;
+
+namespace SocketIOClient.UnitTests.V2.Session.WebSocket;
+
+public class WebSocketSessionTests
+{
+    public WebSocketSessionTests(ITestOutputHelper output)
+    {
+        _wsAdapter = Substitute.For<IWebSocketAdapter>();
+        _uriConverter = Substitute.For<IUriConverter>();
+        _uriConverter.GetServerUri(true, Arg.Any<Uri>(), Arg.Any<string>(),
+                Arg.Any<IEnumerable<KeyValuePair<string, string>>>(), Arg.Any<int>())
+            .Returns(new Uri("ws://localhost:3000/socket.io"));
+        var logger = output.CreateLogger<WebSocketSession>();
+        _session = new WebSocketSession(
+            logger,
+            _wsAdapter,
+            _uriConverter)
+        {
+            Options = _sessionOptions,
+        };
+    }
+
+    private readonly SessionOptions _sessionOptions = new()
+    {
+        ServerUri = new Uri("http://localhost:3000"),
+        Query = new List<KeyValuePair<string, string>>(),
+        EngineIO = EngineIO.V4,
+    };
+
+    private readonly WebSocketSession _session;
+    private readonly IWebSocketAdapter _wsAdapter;
+    private readonly IUriConverter _uriConverter;
+
+
+    #region ConnectAsync
+
+    [Fact]
+    public async Task ConnectAsync_AdapterThrowAnyException_SessionThrowConnectionFailedException()
+    {
+        _wsAdapter
+            .ConnectAsync(Arg.Any<Uri>(), Arg.Any<CancellationToken>())
+            .Throws(new WebSocketException("Server refused connection"));
+
+        await _session.Invoking(async x => await x.ConnectAsync(CancellationToken.None))
+            .Should()
+            .ThrowExactlyAsync<ConnectionFailedException>()
+            .WithMessage("Failed to connect to the server")
+            .WithInnerException(typeof(WebSocketException))
+            .WithMessage("Server refused connection");
+    }
+
+    [Fact]
+    public async Task ConnectAsync_WhenCalled_PassCorrectArgsToAdapter()
+    {
+        await _session.ConnectAsync(CancellationToken.None);
+
+        var expectedUri = new Uri("ws://localhost:3000/socket.io");
+        await _wsAdapter.Received().ConnectAsync(expectedUri, CancellationToken.None);
+    }
+
+    [Fact]
+    public async Task ConnectAsync_UriConverterThrow_PassThroughException()
+    {
+        _uriConverter.GetServerUri(Arg.Any<bool>(), Arg.Any<Uri>(), Arg.Any<string>(),
+                Arg.Any<IEnumerable<KeyValuePair<string, string>>>(), Arg.Any<int>())
+            .Throws(new Exception("UriConverter Error"));
+
+        await _session.Invoking(async x => await x.ConnectAsync(CancellationToken.None))
+            .Should()
+            .ThrowExactlyAsync<Exception>()
+            .WithMessage("UriConverter Error");
+    }
+
+    [Fact]
+    public async Task ConnectAsync_CancelledToken_ThrowConnectionFailedException()
+    {
+        _wsAdapter
+            .ConnectAsync(Arg.Any<Uri>(), Arg.Is<CancellationToken>(t => t.IsCancellationRequested))
+            .Throws(new TaskCanceledException("Task was canceled"));
+
+        await _session.Invoking(async x =>
+            {
+                using var cts = new CancellationTokenSource();
+                await cts.CancelAsync();
+                await x.ConnectAsync(cts.Token);
+            })
+            .Should()
+            .ThrowExactlyAsync<ConnectionFailedException>()
+            .WithMessage("Failed to connect to the server")
+            .WithInnerException(typeof(TaskCanceledException))
+            .WithMessage("Task was canceled");
+    }
+
+    [Fact]
+    public async Task ConnectAsync_2ExtraHeaders_SetDefaultHeaderTwice()
+    {
+        _sessionOptions.ExtraHeaders = new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+            { "key2", "value2" },
+        };
+
+        await _session.ConnectAsync(CancellationToken.None);
+
+        _wsAdapter.Received(1).SetDefaultHeader("key1", "value1");
+        _wsAdapter.Received(1).SetDefaultHeader("key2", "value2");
+    }
+
+    #endregion
+}
