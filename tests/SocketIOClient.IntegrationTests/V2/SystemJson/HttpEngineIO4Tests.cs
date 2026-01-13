d051f6a6bfedb95ea2651cee581c374a4ca8320a
doghappy <hero_wong@outlook.com>
2026-01-13 23:08:30 +0800

    add protocol auto upgrading tests

--- tests/SocketIOClient.IntegrationTests/V2/SystemJson/HttpEngineIO4Tests.cs
+++ tests/SocketIOClient.IntegrationTests/V2/SystemJson/HttpEngineIO4Tests.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using FluentAssertions;
 using SocketIOClient.Core;
+using SocketIOClient.V2.Session.WebSocket;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace SocketIOClient.IntegrationTests.V2.SystemJson;
@@ -16,4 +21,19 @@ public class HttpEngineIO4Tests(ITestOutputHelper output) : SocketIOEngineIO4Tes
         Reconnection = false,
         ConnectionTimeout = TimeSpan.FromSeconds(5),
     };
+
+    [Fact]
+    public async Task ConnectAsync_WebSocketIsAvailable_UpgradeToWebSocket()
+    {
+        var uri = new Uri("http://localhost.charlesproxy.com:11400");
+        var io = NewSocketIO(uri);
+
+        await io.ConnectAsync();
+
+        io.Options.Transport.Should().Be(TransportProtocol.WebSocket);
+
+        var prop = io.GetType().GetField("_session", BindingFlags.Instance | BindingFlags.NonPublic);
+        var session = prop!.GetValue(io);
+        session.Should().BeOfType<WebSocketSession>();
+    }
 }
