d462c3bbd92843b812260262f83c1f6746b07237
George Legun <georgelegun@gmail.com>
2026-04-17 21:07:08 +0300

    await ReceiveAsync(token) in WS ConnectAsync
    - not awaiting this would cause an unhandled exception, because of the throw at the end of ReceiveAsync

--- src/SocketIOClient/Protocol/WebSocket/WebSocketAdapter.cs
+++ src/SocketIOClient/Protocol/WebSocket/WebSocketAdapter.cs
@@ -16,7 +16,8 @@ public class WebSocketAdapter(ILogger<WebSocketAdapter> logger, IWebSocketClient
     {
         await clientAdapter.ConnectAsync(uri, cancellationToken).ConfigureAwait(false);
         var token = _receiveCancellationTokenSource.Token;
-        _ = ReceiveAsync(token).ConfigureAwait(false);
+        // must either be awaited or ReceiveAsync must not throw
+        await ReceiveAsync(token).ConfigureAwait(false);
     }
 
     private async Task ReceiveAsync(CancellationToken cancellationToken)
